9b24ffa49c38f1c9ac0bd18e4e2a243f60a66069
Matthew McKenzie <mmckenzie3@elon.edu>
2013-03-19 13:57:15 -0400

    blue commit

--- src/edu/elon/cs/elon/locationexposure/LocationExposure.java
+++ src/edu/elon/cs/elon/locationexposure/LocationExposure.java
@@ -17,7 +17,7 @@ public class LocationExposure extends Activity {
 	private RelativeLayout relativeLayout;
 	private LocationManager locManager;
 	
-	private int[] colors = {Color.RED, Color.GREEN};
+	private int[] colors = {Color.BLUE, Color.GREEN};
 	private int which;
 	
 	@Override
